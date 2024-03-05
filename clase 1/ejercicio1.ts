8de508b54cc40bce1e7173c8db8ba8deb13dcbfc
carlavgonzalez25 <carla.v.gonzalez@hotmail.com>
2024-03-05 14:58:59 -0300

    first commit

---
+++ clase 1/ejercicio1.ts
@@ -0,0 +1,22 @@
+/* 
+Factorial de un número:
+Descripción: Calcular el factorial de un número entero no negativo n, denotado por n!. El factorial de un número n se define como el producto de todos los enteros positivos menores o iguales a n.
+Entrada: Un número entero no negativo n. x ej: 5
+Salida: El factorial de n, es decir, n!. Salida: 5x4x3x2x1 = 120
+*/
+
+// Enfoque 1
+function calculateFactorial(num: number): number {
+  let result = num;
+  for (let i = num - 1; i > 0; i--) {
+    result = result + i;
+  }
+  return result;
+}
+
+// Enfoque 2, usando recursividad
+function calculateFactorial2(num: number, resultado: number = 0): number {
+  if (num <= 0) return resultado;
+  resultado = resultado + num;
+  return calculateFactorial2(num - 1, resultado);
+}
